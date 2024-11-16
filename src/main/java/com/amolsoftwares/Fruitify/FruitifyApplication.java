a398130e5d2e929001f94e677f63b17a11ba5b61
Amol <amolgadage663@gmail.com>
2024-11-16 20:39:38 +0530

    initial commit

---
+++ src/main/java/com/amolsoftwares/Fruitify/FruitifyApplication.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.Fruitify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amolsoftwares/Fruitify/FruitifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.Fruitify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
